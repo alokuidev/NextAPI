d8293f320290ea0dc31ddd2f06f5bba6d748b0fb
warrioralok <34855411+warrioralok@users.noreply.github.com>
2024-12-30 19:00:31 +0530

    post method added

--- src/app/api/users/route.js
+++ src/app/api/users/route.js
@@ -5,3 +5,13 @@ export function GET(){
     const data = student
     return NextResponse.json(data,{status:200})
 }
+
+export async function POST(request){
+    let payload = await request.json();
+    console.log(payload.name)
+    if(!payload.name || !payload.age){
+        return NextResponse.json({reult:'Result Not Found'},{status:'400'})    
+    }
+    return NextResponse.json({reult:'New User Created'},{status:201})
+
+}
