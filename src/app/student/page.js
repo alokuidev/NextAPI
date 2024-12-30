1531219082e8904f41ef5c9635dd3a7a38b9d5b0
warrioralok <34855411+warrioralok@users.noreply.github.com>
2024-12-30 18:22:02 +0530

    api calls

---
+++ src/app/student/page.js
@@ -0,0 +1,55 @@
+/* eslint-disable react-hooks/rules-of-hooks */
+'use client'
+import { useEffect, useState } from "react";
+import styles from "./page.module.css";
+import Link from "next/link";
+const student = () => {
+  const [studentData, setStudentData] = useState([])
+  const apiCall = async () =>{
+    try {
+        const response = await fetch('http://localhost:3000/api/users');
+        const data = await response.json();
+        setStudentData(data)
+    } catch (error) {
+        console.warn(`Some Error Occurd: ${error}`)
+    }
+  }  
+
+  useEffect(() => {
+    apiCall();
+  }, []);
+
+  return (
+    <>
+      <h1 className={styles.heading}>Student List</h1>
+      <table className={styles.studentList}>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Age</th>
+            <th>Grade</th>
+            <th>Subjects</th>
+          </tr>
+        </thead>
+        <tbody className={styles.studentTable}>
+            
+        {studentData.map((elem) =>{
+            const subLength = elem.subjects.length;
+            return(   
+           
+           <tr key={elem.id}>
+                <td><Link href={`student/${elem.id}`}>{elem.id}</Link></td>
+                <td>{elem.name}</td>
+                <td>{elem.age}</td>
+                <td>{elem.grade}</td>
+                <td>{elem.subjects.map((sub,index) =>  (subLength -1 === index) ? `${sub}` : `${sub} ,` )}</td>
+            </tr>)
+            })}
+        </tbody>
+      </table>
+    </>
+  );
+};
+
+export default student;
